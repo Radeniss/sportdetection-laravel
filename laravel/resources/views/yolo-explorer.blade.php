71d4c59c2ab37d729d3910903cd1b8b258c1469e
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-14 16:48:44 +0700

    update

--- laravel/resources/views/yolo-explorer.blade.php
+++ laravel/resources/views/yolo-explorer.blade.php
@@ -33,6 +33,20 @@
         prevSlide() {
             this.carouselIndex = this.carouselIndex === 0 ? 1 : 0;
         },
+        resetUpload() {
+            this.videoFile = null;
+            this.$refs.fileInput.value = '';
+        },
+        toggleCamera() {
+            this.cameraActive = !this.cameraActive;
+        },
+        submitVideo() {
+            if (this.videoFile) {
+                alert('Video berhasil dikirim: ' + this.videoFile);
+            } else {
+                alert('Silakan pilih video terlebih dahulu');
+            }
+        }
     }" class="max-w-7xl mx-auto px-4 py-8">
 
         @include('partials.header')
