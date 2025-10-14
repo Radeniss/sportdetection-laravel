b22221efdd7690c408fc1c77de562978f2974c58
Radeniss~ <radenis354@gmail.com>
2025-10-14 18:19:21 +0700

    update

--- laravel/resources/views/tabs/detection-input.blade.php
+++ laravel/resources/views/tabs/detection-input.blade.php
@@ -16,7 +16,7 @@
                             d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                     </svg>
                     <h3 class="text-2xl font-bold text-slate-800 mb-2">Upload Video</h3>
-                    <p class="text-slate-600">Unggah file video dari perangkat Anda untuk diproses</p>
+                    <p class="text-slate-600">Unggah file video dari perangkat Anda untuk diprosesssss</p>
                 </div>
 
                 <div class="bg-white rounded-lg p-6">
